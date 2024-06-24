c105a7dbdf97a467bfd21d26e9971c1b8a525fa0
Aleixo Gerhardt <aleixogerhardt@gmail.com>
2024-06-24 19:31:44 -0300

    Adicionando pasta do dia 4

---
+++ dia04/index.js
@@ -0,0 +1,67 @@
+// if else escopo
+// == !=  >  <  >=  <=
+// === é igual o valor e o tipo I !== é diferente o valor e o tipo
+
+// let idade = Number(prompt('Insira sua idade: '))
+
+// let condicao = true // or false
+// if(idade <= 10){
+//     console.log('Você tem MENOS de 10 anos')
+// }else{
+//     console.log('Você tem MAIS de 10 anos')
+// }
+
+// ------------------------------------------
+
+// let idade = Number(prompt('Insira sua idade: '))
+// let nome = prompt('Insira seu nome: ')
+
+// if(idade === 20 && nome === "Aleixo"){
+//     console.log('Seu nome é Aleixo e você tem 20 anos.')
+// }
+
+// if(idade === 20 || nome === "Aleixo"){
+//     console.log('Seu nome é Aleixo ou você tem 20 anos.')
+// }else{
+//     console.log('Seu nome não é Aleixo e você não tem 20 anos.')
+// }
+// --------------------------------------------------------------------------------------------------------------------------------
+
+// // ATIVIDADE PROPOSTA NA AULA
+// !!! Tire do comentário caso queira testar !!!
+// let perguntaFome = prompt('Você tá com fome? (sim/nao) ')
+// let perguntaDinheiro = prompt('Você tem dinheiro? (sim/nao)')
+// let perguntaRestaurante = prompt('O Restaurante está aberto? (sim/nao)')
+
+// if(perguntaFome === 'nao' || perguntaDinheiro === `nao`){
+//     console.log('Hoje a janta será em casa.')
+// }
+// if(perguntaFome === 'sim' && perguntaDinheiro === 'sim' && perguntaRestaurante === 'nao'){
+//     console.log('Pede no iFood!')
+// }
+// if(perguntaFome === 'sim' && perguntaDinheiro === 'sim' && perguntaRestaurante === 'sim'){
+//     console.log('Hoje o jantar será no restaurante.')
+// }
+// !!! Tire do comentário caso queira testar !!!
+
+// --------------------------------------------------------------------------------------------------------------------------------
+
+// // ATIVIDADE PROPOSTA NA AULA
+// !!! Tire do comentário caso queira testar !!!
+// const infoNome = prompt('Qual o seu nome? ')
+// const infoIdade = Number(prompt('Qual a sua idade? '))
+// const infoCarteira = prompt('Você possui Carteira de Motorista? (sim/nao)')
+// const infoCarro = prompt('Você possui algum carro? (sim/nao)')
+
+// if(infoIdade < 18 || infoCarteira === 'nao'){
+//     console.log('Você não pode dirigir.')
+// }
+// if(infoIdade >= 18 && infoCarteira === 'sim' && infoCarro === 'nao'){
+//     console.log('Você pode dirigir mas não tem um carro.')
+// }
+// if(infoIdade >= 18 && infoCarteira === 'sim' && infoCarro === 'sim'){
+//     console.log('Você será o motorista de hoje.')
+// }
+// !!! Tire do comentário caso queira testar !!!
+
+// --------------------------------------------------------------------------------------------------------------------------------
